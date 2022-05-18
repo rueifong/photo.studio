cedaff39f566286b62530c67a91844f9d668451b
Rueifong <karta833@gmail.com>
2022-05-18 15:32:55 +0800

    移除多餘的內容

--- js/color-switcher.js
+++ js/color-switcher.js
@@ -66,19 +66,19 @@ jQuery(document).ready(function($) {
 jQuery('head').append('<link rel="stylesheet" id="colors" href="css/colors/orange.css" type="text/css" />');
 jQuery('head').append('<link rel="stylesheet" href="css/color-switcher.css" type="text/css" />'); 
 
-jQuery('body').append('' + 
-	'<div id="color-style-switcher">' +
-		'<div>' + 
-			'<h3>Color Palette</h3>' +
-			'<ul class="colors">' +
-				'<li><a class="color1" href="#"></a></li>' +
-				'<li><a class="color2" href="#"></a></li>' +
-				'<li><a class="color3" href="#"></a></li>' +
-				'<li><a class="color4 active" href="#"></a></li>' +
-				'<li><a class="color5" href="#"></a></li>' +
-				'<li><a class="color6" href="#"></a></li>' +
-			'</ul>' +
-		'</div>' +
-		'<div class="bottom"> <a href="#" class="settings"><i class="lni-cog"></i></a> </div>' +
-	'</div>' +
-'');
+// jQuery('body').append('' + 
+// 	'<div id="color-style-switcher">' +
+// 		'<div>' + 
+// 			'<h3>Color Palette</h3>' +
+// 			'<ul class="colors">' +
+// 				'<li><a class="color1" href="#"></a></li>' +
+// 				'<li><a class="color2" href="#"></a></li>' +
+// 				'<li><a class="color3" href="#"></a></li>' +
+// 				'<li><a class="color4 active" href="#"></a></li>' +
+// 				'<li><a class="color5" href="#"></a></li>' +
+// 				'<li><a class="color6" href="#"></a></li>' +
+// 			'</ul>' +
+// 		'</div>' +
+// 		'<div class="bottom"> <a href="#" class="settings"><i class="lni-cog"></i></a> </div>' +
+// 	'</div>' +
+// '');
